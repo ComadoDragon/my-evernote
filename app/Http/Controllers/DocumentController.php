cf5e3af44b9c3e726acf4f4c1a3c31ca4e1ddb5a
Gökmen Saral <gokmensaral@gmail.com>
2024-11-11 14:18:53 +0300

    document index method view create

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Document;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DocumentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('documents.index');
     }
 
     /**
