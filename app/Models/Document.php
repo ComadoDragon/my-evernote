bc53cacead6497ae3e18b4986068feab5368df9c
Gökmen Saral <gokmensaral@gmail.com>
2024-11-11 16:23:15 +0300

    Connecting users to Document and Updating our view

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
     protected $fillable = ['title', 'description'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
