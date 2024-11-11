ca25421fbd84cb105f668cdbd86d6380828d5273
Gökmen Saral <gokmensaral@gmail.com>
2024-11-11 15:07:27 +0300

    document model add foreignId User id

--- database/migrations/2024_11_10_193435_create_documents_table.php
+++ database/migrations/2024_11_10_193435_create_documents_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->char('title', length: 250);
             $table->text('description');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
